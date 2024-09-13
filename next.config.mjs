e49b9f1c2ddcb7ad58e9c9ea4ff0b60c1d50247e
VanAug <okellobrian18@gmail.com>
2024-09-13 11:13:10 +0300

    Create a community page.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   serverActions: true,
   mdx: true,
   serverComponentsExternalPackages: ["mongoose"],
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
