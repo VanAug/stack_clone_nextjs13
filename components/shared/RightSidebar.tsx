a19f1423b11833f43850d98dd85a79a15f7f6670
VanAug <okellobrian18@gmail.com>
2024-09-20 09:26:57 +0300

    Get Hot Questions and Popular tags.

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,24 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getPopularTags } from "@/lib/actions/tag.actions";
 
-const hotQuestions = [
-  { _id: "1", title: "How do i pass time " },
-  { _id: "2", title: "How do i pass time " },
-  { _id: "3", title: "How do i pass time " },
-  { _id: "4", title: "How do i pass time " },
-  { _id: "5", title: "How do i pass time " },
-];
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getPopularTags();
 
-const popularTags = [
-  { _id: "1", name: "JavaScript", totalQuestions: 32 },
-  { _id: "2", name: "React", totalQuestions: 24 },
-  { _id: "3", name: "Next", totalQuestions: 53 },
-  { _id: "4", name: "vue", totalQuestions: 39 },
-  { _id: "5", name: "Redux", totalQuestions: 29 },
-];
-
-const RightSidebar = () => {
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col  overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
@@ -27,7 +16,7 @@ const RightSidebar = () => {
         <div className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions.map((question) => (
             <Link
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               key={question._id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
@@ -53,7 +42,7 @@ const RightSidebar = () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
